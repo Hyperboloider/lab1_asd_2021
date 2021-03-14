2f1148f2947bfc56edfbbaf50426804bce9c2e73
Ilya Knyazev <kknyaz033@gmail.com>
2021-03-14 22:17:53 +0200

    added correct code

--- lab1_2.py
+++ lab1_2.py
@@ -12,25 +12,31 @@ def input_matrix():
 
 def hamilton(graph, current, seen = None, path = None):
     if path == None:                                               #first call creates auxiliary structures
-        seen = [False] * len(graph)
+        seen = set()
         path = [current,]
+        print(current + 1)
     else:
         path.append(current)
         print("-".join([str(i + 1) for i in path]))
+        
     if len(path) == len(graph):
         if graph[path[0]][path[-1]] == 1:                          #the head and the tail of the path have common edge
             print(colored("->".join([str(i + 1) for i in path]) + f"->{path[0] + 1}", "green"))
             return True
         else:
             path.pop()
+            print("-".join([str(i + 1) for i in path]))
             return False
-    seen[current] = True
+            
+    seen.add(current)
+    
     for next_node in range(len(graph)):
-        if graph[current][next_node] == 1 and not seen[next_node]: #the node has common edge with the current one and is not included into the current path
+        if graph[current][next_node] == 1 and not (next_node in seen): #the node has common edge with the current one and is not included into the current path
             if hamilton(graph, next_node, seen, path):
                 return True
-    seen[current] = False
+    seen.remove(current)
     path.pop()
+    print("-".join([str(i + 1) for i in path]))
     return False
 
 start = int(input('Strart node: ')) - 1
